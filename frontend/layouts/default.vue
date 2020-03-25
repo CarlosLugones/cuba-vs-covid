068a0c586f77e45903878485346880c02305df8d
Carlos Lugones <contact@lugodev.com>
2020-03-25 17:03:45 -0400

    Initial commit

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nav
+      class="navbar header has-shadow is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+          <img src="#" alt="" height="28" />
+        </a>
+
+        <div class="navbar-burger">
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
+    </nav>
+
+    <section class="main-content">
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'index.vue'
+}
+</script>
+
+<style scoped></style>
