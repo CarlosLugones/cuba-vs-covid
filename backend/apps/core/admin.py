90581ee2b1db03687d8eb80f0ebed8aa41d8131f
Carlos Lugones <contact@lugodev.com>
2020-03-25 17:47:41 -0400

    add: Added base admin classes in core

---
+++ backend/apps/core/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Province)
+class ProvinceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.City)
+class CityAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Address)
+class AddressAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Workshop)
+class WorkshopAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.STLModel)
+class STLModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
