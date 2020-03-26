081315427b674d9dad706560ba1c12793710b75a
Carlos Lugones <contact@lugodev.com>
2020-03-25 20:08:21 -0400

    add: Added impression config static page

---
+++ frontend/pages/pages/impression-config.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <h1 class="title">Parámetros para viceras</h1>
+    <p>Altura de capa 0.3</p>
+    <p>
+      Ancho ~0.36👈clave!!, ya que todas se imprimen con gaps pequeños entre
+      paredes y mientras mas pequeño el ancho de la extrusion más limpieza y más
+      pases en una misma pared👈mejor propiedades mecánicas
+    </p>
+    <p>
+      Relleno 👈esto hay que probarlo,hay que buscar un equilibrio y darle
+      estructuralmente una resiliencia al PLA que no tiene, 👈si me preguntan
+      Gyroid o Honeycomb ~30% Buscar los enlaces que ha subido
+      @Guillermo_Ferrer👈dede españa
+    </p>
+    <p>
+      Velocidad 👈empiecen con 60mm/s, si todo va bien , le meten 70👈OJO ,
+      mirando como se desempeña en las primeras ~100 capas
+    </p>
+    <p>
+      Retracción 👈eso es específico de cada máquina 👈como promedio
+    </p>
+    <p>
+      Bowden entre 3 ~5mm , direct 1~2mm
+    </p>
+    <p>
+      Temp 👈igual especifico, aqui todo el mundo conoce como pincha su máquina
+      con PLA
+    </p>
+    <p>
+      Velocidad de retracción 👈especifico, como promedio Bowden de 30~45mm/
+    </p>
+    <p>Direct de 20~30mm/s</p>
+    <p>
+      Retraer solo cuando el extrusor cruce espacios 👈esto se imprime casi en
+      modo Vase, por lo que se ahorra su poco marcando eso
+    </p>
+    <p>
+      Ventilación activa 100%👈sobre todo a 70mm/s 👈hay que secar rápido la
+      capa anterior
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
