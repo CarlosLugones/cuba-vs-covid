21bf429aad808f13b3b3ba97419b4a21e5a6dc02
Carlos Lugones <contact@lugodev.com>
2020-04-10 12:45:46 -0400

    add: Added product lists

--- frontend/pages/dashboard/products/index.vue
+++ frontend/pages/dashboard/products/index.vue
@@ -6,12 +6,53 @@
     >
       Añadir producto
     </b-button>
+    <b-table :data="products">
+      <template slot-scope="props">
+        <b-table-column field="id" label="Nombre">
+          {{ props.row.name }}
+        </b-table-column>
+        <b-table-column field="id" label="Inventario">
+          {{ props.row.stock }}
+        </b-table-column>
+        <b-table-column label="Acciones">
+          <b-button>
+            <b-icon icon="pencil"></b-icon>
+          </b-button>
+          <b-button>
+            <b-icon icon="delete"></b-icon>
+          </b-button>
+        </b-table-column>
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
 export default {
-  layout: 'dashboard'
+  layout: 'dashboard',
+  data() {
+    return {
+      products: []
+    }
+  },
+  mounted() {
+    this.$apollo
+      .query({
+        query: gql`
+          query viewerProducts {
+            viewerProducts {
+              id
+              name
+              stock
+            }
+          }
+        `
+      })
+      .then(({ data }) => {
+        this.products = data.viewerProducts
+      })
+  }
 }
 </script>
 
