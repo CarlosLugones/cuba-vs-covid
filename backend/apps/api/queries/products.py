79c5bc43713af59a55a6bd7b6fec896a48e33383
Carlos Lugones <contact@lugodev.com>
2020-04-10 12:44:56 -0400

    feat: List owned products

--- backend/apps/api/queries/products.py
+++ backend/apps/api/queries/products.py
@@ -1,14 +1,14 @@
 import graphene
 
 from apps.api.auth import authenticate
-from apps.api.types.workshops import WorkshopType
+from apps.api.types.products import ProductType
 
 
 class ProductsQuery:
-    viewer_products = graphene.Field(WorkshopType)
+    viewer_products = graphene.List(ProductType)
 
     def resolve_viewer_products(self, info):
         user = authenticate(info.context)
         if user is not None:
-            return user.workshop_set.first()
+            return user.product_set.all()
         return None
